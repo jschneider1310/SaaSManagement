86b246d4b31807150d3e12fdcc9aa9fd1365fe17
Jeferson Giovani D. Schneider <j.g@live.com>
2026-05-21 12:20:58 +0100

    Added: Created the ClientId.cs value object that represents the customer's Ids.

---
+++ SaaSManagement.Core/Features/CustomerManagement/Primitives/ClientId.cs
@@ -0,0 +1,30 @@
+// Project: SaaSManagement, 21/05/2026
+// Author: J. Schneider - j.g@live.com
+
+using System.ComponentModel.DataAnnotations;
+using SaaSManagement.Core.Shared.Constants;
+using ApplicationId = SaaSManagement.Core.Shared.Abstractions.ApplicationId;
+
+namespace SaaSManagement.Core.Features.CustomerManagement.Primitives;
+/// <summary>
+/// Value Object class that represents an ID for any type of customer in this application.
+/// </summary>
+internal sealed class ClientId : ApplicationId
+{
+    [MaxLength(CoreConstants.ApplicationIdsMaxCharacters)]
+    protected override string Value { get; set; }
+
+
+    public ClientId(string id)
+    {
+        Value = id;
+    }
+
+    public static implicit operator string(ClientId clientId) => clientId.Value;
+    public static implicit operator ClientId(string clientId) => new(clientId);
+
+    protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+}
