faf9054dd9b7b1d970bf435f450d7239cfc36dc9
Jeferson Giovani D. Schneider <j.g@live.com>
2026-05-21 20:54:57 +0100

    Refactor: Folder structure changed Added: IServiceManagementRepository.cs with no inheritance from IRepository yet

---
+++ SaaSManagement.Core/ServicesManagement/Repository/IServiceManagementRepository.cs
@@ -0,0 +1,11 @@
+// Project: SaaSManagement, 21/05/2026
+// Author: J. Schneider - j.g@live.com
+
+using SaaSManagement.Core.Shared.Abstractions.Interfaces;
+
+namespace SaaSManagement.Core.ServicesManagement.Repository;
+
+public interface IServiceManagementRepository
+{
+    
+}
