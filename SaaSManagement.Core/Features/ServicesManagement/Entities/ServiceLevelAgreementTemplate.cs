e9b5ce35da046b7bc2895a0cbd2b77b39a4a7be7
Jeferson Giovani D. Schneider <j.g@live.com>
2026-05-21 15:54:56 +0100

    Created: Shared utility for common parameter verifications in VerifyIf.cs

--- SaaSManagement.Core/Features/ServicesManagement/Entities/ServiceLevelAgreementTemplate.cs
+++ SaaSManagement.Core/Features/ServicesManagement/Entities/ServiceLevelAgreementTemplate.cs
@@ -1,7 +1,6 @@
 // Project: SaaSManagement, 21/05/2026
 // Author: J. Schneider - j.g@live.com
 
-using System.Collections.ObjectModel;
 using SaaSManagement.Core.Features.ServicesManagement.Records;
 using SaaSManagement.Core.Shared.Abstractions;
 
@@ -9,7 +8,7 @@ namespace SaaSManagement.Core.Features.ServicesManagement.Entities;
 
 public sealed class ServiceLevelAgreementTemplate : Entity<string>
 {
-    public ServiceLevelAgreementTemplate(string title, ServiceOverview serviceOverview,
+    public ServiceLevelAgreementTemplate(string? title, ServiceOverview serviceOverview,
         ServiceAvailability serviceAvailability,
         List<AvailabilityDefinition> availabilityDefinitions,
         List<OutOfScopeThirdParty> outOfScopeThirdParty,
@@ -17,7 +16,7 @@ public sealed class ServiceLevelAgreementTemplate : Entity<string>
         List<RecoveryContext> recoveryContext,
         List<AvailabilityExclusion> availabilityExclusion)
     {
-        Title = title;
+        Title = title ?? "Service-Level Agreement";
         ServiceOverview = serviceOverview;
         ServiceAvailability = serviceAvailability;
         AvailabilityDefinitions = availabilityDefinitions;
---
+++ SaaSManagement.Core/Shared/Utilities/VerifyIf.cs
@@ -0,0 +1,64 @@
+// Project: SaaSManagement, 21/05/2026
+// Author: J. Schneider - j.g@live.com
+
+using System.Net.Mail;
+
+namespace SaaSManagement.Core.Shared.Utilities;
+
+public static class VerifyIf
+{
+    /// <summary>
+    /// Verifies if a given string representing an e-mail address is valid.
+    /// </summary>
+    /// <param name="email">String</param>
+    /// <returns>True if the email is valid, and false otherwise.</returns>
+    public static bool IsValidEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email)) return false;
+        if (email.Contains(' ')) return false;
+
+        if (!MailAddress.TryCreate(email, out var mailAddress)) return false;
+
+        var hostParts = mailAddress.Host.Split('.');
+
+        // No dot.
+        if (hostParts.Length < 2) return false;
+        // Double dot.
+        if (hostParts.Any(part => part == string.Empty)) return false;
+        // TLD only one character.
+        if (hostParts[^1].Length < 2) return false;
+        //Space in user part.
+        if (mailAddress.User.Contains(' ')) return false;
+        //Double dot or dot at the end of user part.
+        if (mailAddress.User.Split('.').Any(p => p == string.Empty)) return false;
+
+        return true;
+    }
+    /// <summary>
+    /// Verifies if a given integer is greater or equal zero.
+    /// </summary>
+    /// <param name="value">Integer</param>
+    /// <returns>True if the integer is zero or positive, and false if it is negative.</returns>
+    public static bool IsPositiveInteger(int value)
+    {
+        return value >= 0;
+    }
+    /// <summary>
+    /// Verifies if a given double is greater or equal to zero.
+    /// </summary>
+    /// <param name="value">Double</param>
+    /// <returns>True if it is greater or equal zero, and false otherwise.</returns>
+    public static bool NegativeDouble(double value)
+    {
+        return value >= 0.0;
+    }
+    /// <summary>
+    /// Verifies if a given string value is NOT null, empty, or white spaces.
+    /// </summary>
+    /// <param name="value">String</param>
+    /// <returns>True if it is not null or empty or white spaces, and false otherwise.</returns>
+    public static bool IsNotEmptyOrNullString(string value)
+    {
+        return !string.IsNullOrWhiteSpace(value);
+    }
+}
