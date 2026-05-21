a780f3b7d67014bf81e6615ddea9e10700a0752e
Jeferson Giovani D. Schneider <j.g@live.com>
2026-05-21 13:35:35 +0100

    Added: ServiceType.cs that represents if a given service is Internal or External (third-party/partner service).

---
+++ SaaSManagement.Core/Features/ServicesManagement/Constants/ServiceType.cs
@@ -0,0 +1,10 @@
+// Project: SaaSManagement, 21/05/2026
+// Author: J. Schneider - j.g@live.com
+
+namespace SaaSManagement.Core.Features.ServicesManagement.Constants;
+
+public enum ServiceType
+{
+    Internal, // Provided by the company
+    External // Provided by third-party or partners
+}
