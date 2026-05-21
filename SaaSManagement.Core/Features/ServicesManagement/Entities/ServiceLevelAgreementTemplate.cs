3fa0369ae9fe7cc72f8a955f0e5d66c0974c509a
Jeferson Giovani D. Schneider <j.g@live.com>
2026-05-21 15:57:35 +0100

    Fix: Missing documentation and import

--- SaaSManagement.Core/Features/ServicesManagement/Entities/ServiceLevelAgreementTemplate.cs
+++ SaaSManagement.Core/Features/ServicesManagement/Entities/ServiceLevelAgreementTemplate.cs
@@ -1,11 +1,15 @@
 // Project: SaaSManagement, 21/05/2026
 // Author: J. Schneider - j.g@live.com
 
+using SaaSManagement.Core.Features.CustomerManagement.Entities;
 using SaaSManagement.Core.Features.ServicesManagement.Records;
 using SaaSManagement.Core.Shared.Abstractions;
 
 namespace SaaSManagement.Core.Features.ServicesManagement.Entities;
-
+/// <summary>
+/// Class that represents a template to be used to generate a new <see cref="ServiceLevelAgreement"/>
+/// for a particular <see cref="Customer"/>
+/// </summary>
 public sealed class ServiceLevelAgreementTemplate : Entity<string>
 {
     public ServiceLevelAgreementTemplate(string? title, ServiceOverview serviceOverview,
