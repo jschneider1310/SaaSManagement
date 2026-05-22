39c14d7c5a499a2dff0d4dea2633004a894aae2b
Jeferson Giovani D. Schneider <j.g@live.com>
2026-05-22 20:04:22 +0100

    Added: BusinessCustomer.cs Doc: Aggregate root Customer.cs explained

---
+++ SaaSManagement.Core/CustomerManagement/Entities/BusinessCustomer.cs
@@ -0,0 +1,9 @@
+// Project: SaaSManagement, 22/05/2026
+// Author: J. Schneider - j.g@live.com
+
+namespace SaaSManagement.Core.CustomerManagement.Entities;
+
+public sealed class BusinessCustomer : Customer
+{
+    
+}
--- SaaSManagement.Core/CustomerManagement/Entities/Customer.cs
+++ SaaSManagement.Core/CustomerManagement/Entities/Customer.cs
@@ -6,10 +6,16 @@ using SaaSManagement.Core.Shared.Abstractions;
 using SaaSManagement.Core.Shared.Abstractions.Classes;
 
 namespace SaaSManagement.Core.CustomerManagement.Entities;
-
+/// <summary>
+/// Represents a Customer into the system. It is the aggregate root for different
+/// types of customers (<see cref="IndividualCustomer"/> and <see cref="BusinessCustomer"/>).
+/// Here we placed all common fields for a customer, leaving the specific implementations
+/// of each type for its own responsibility.
+/// </summary>
 public class Customer : AggregateRoot<ClientId>
 {
     public new ClientId Id { get; private set; }
+    
     
     
     public override string GetId() => Id;
