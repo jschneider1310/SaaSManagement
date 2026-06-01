6120101372e43ce9acd4d096cccd6c5156e7e9ef
Jeferson Giovani D. Schneider <j.g@live.com>
2026-06-01 20:44:56 +0100

    Refactor: Emptied the default interface for DbContext (IApplicationDbContext.cs) and created feature based interface ICustomerManagementDbContext.cs

---
+++ SaaSManagement.Core/CustomerManagement/Domain/Context/ICustomerManagementDbContext.cs
@@ -0,0 +1,11 @@
+// Project: SaaSManagement, 01/06/2026
+// Author: J. Schneider - j.g@live.com
+
+using SaaSManagement.Core.Shared.Abstractions.Interfaces;
+
+namespace SaaSManagement.Core.CustomerManagement.Domain.Context;
+
+public interface ICustomerManagementDbContext : IApplicationDbContext
+{
+    
+}
--- SaaSManagement.Core/Shared/Abstractions/Interfaces/IApplicationDbContext.cs
+++ SaaSManagement.Core/Shared/Abstractions/Interfaces/IApplicationDbContext.cs
@@ -1,23 +1,12 @@
 // Project: SaaSManagement, 27/05/2026
 // Author: J. Schneider - j.g@live.com
 
-using Microsoft.EntityFrameworkCore;
-using SaaSManagement.Core.CustomerManagement.Domain.Entities;
-using SaaSManagement.Core.ServicesManagement.Domain.Entities;
-using SaaSManagement.Core.Shared.Primitives;
-
 namespace SaaSManagement.Core.Shared.Abstractions.Interfaces;
 /// <summary>
 /// Interface that represents the DbContext of the application
 /// </summary>
 public interface IApplicationDbContext
 {
-    DbSet<Customer> Customers { get; set; }
-    DbSet<IndividualCustomer> IndividualCustomers { get; set; }
-    DbSet<BusinessCustomer> BusinessCustomers { get; set; }
-    DbSet<ServiceLevelAgreement> ServiceLevelAgreements { get; set; }
-    DbSet<Address> Addresses { get; set; }
-    DbSet<Note> Notes { get; set; }
     
     /// <summary>
     /// Saves the changes to the database.
